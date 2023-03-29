6603dc9847ff68db671f8526b9e21e6767247c8b
Deliaaan <delian.ngtmr@gmail.com>
2023-03-29 12:42:32 -0600

    falta agregar el p

--- app.js
+++ app.js
@@ -10,15 +10,25 @@ right.addEventListener('click', ()=> {
     cards.scrollLeft += 140;
 })
 
+const descripciones = [
+    "Hola como estas", 
+    "sabe a coco",
+    "sabe muy rica jajaja",
+    "esta es de nuestras favoritas"
+]
 
 let poster = document.getElementById('poster');
 let title = document.getElementById('title');
 let price = document.getElementById('price_cont');
+let info = document.getElementById('description');
 
 Array.from(document.getElementsByClassName('card')).forEach((ele, i) => {
     ele.addEventListener('click', () => {
         poster.src = ele.getElementsByTagName('img')[0].src;
         title.src = ele.getElementsByTagName('h5')[0].src;
         price.src = ele.getElementsByTagName('h4')[0].src;
+        info.src = ele.getElementsByTagName('h1')[0].src;
     })
 })
+
+
